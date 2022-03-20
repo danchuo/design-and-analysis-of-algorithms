143c9068c439c716c1da370ddbb914921ab5114b
dontyouo <danchuodanchuo@gmail.com>
2022-03-20 18:31:41 +0300

    9.D re5 v2

--- #9 Encoding and compression/D_HammingDecoding/main.cpp
+++ #9 Encoding and compression/D_HammingDecoding/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <algorithm>
+
+std::string *calculateControlBits(std::string *bits) {
+    auto control_bits = new std::string();
+    int pow_of_two = 0;
+    int unit_counter;
+    for (int i = 0; i < bits->size(); i = std::pow(2, pow_of_two) - 1) {
+        unit_counter = (bits->at(i) == '1') ? 1 : 0;
+        for (int j = i + 2; j <= bits->size(); ++j) {
+            if (j & (1 << pow_of_two)) {
+                if (bits->operator[](j - 1) == '1') {
+                    ++unit_counter;
+                }
+            }
+        }
+
+        if (unit_counter % 2 == 0) {
+            control_bits->push_back('0');
+        } else {
+            control_bits->push_back('1');
+        }
+
+        ++pow_of_two;
+    }
+
+    return control_bits;
+}
 
 void printStringWithoutControlBits(const std::string &string) {
     int pow_of_two = 0;
@@ -13,20 +40,9 @@ void printStringWithoutControlBits(const std::string &string) {
     }
 }
 
-int findPositionWithMistake(const std::string &string) {
-    int mistake = 0;
-
-    for (int i = 0; i < string.length(); ++i) {
-        if (string[i] == '1') {
-            mistake ^= i + 1;
-        }
-    }
-
-    return mistake;
-}
-
-void fixMistake(std::string *string_to_decode) {
-    int index_of_mistake = findPositionWithMistake(*string_to_decode);
+void fixMistake(std::string *string_to_decode, const std::string control_bits) {
+    int index_of_mistake =
+        std::stoi(std::string(control_bits.rbegin(), control_bits.rend()), nullptr, 2);
 
     if (index_of_mistake != 0) {
         string_to_decode->operator[](index_of_mistake - 1) =
@@ -40,8 +56,10 @@ int main() {
     std::string input_line;
     std::getline(std::cin, input_line);
 
-    fixMistake(&input_line);
+    auto control_bits = calculateControlBits(&input_line);
+    fixMistake(&input_line, *control_bits);
     printStringWithoutControlBits(input_line);
 
+    delete control_bits;
     return 0;
 }
