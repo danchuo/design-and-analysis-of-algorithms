0d3bbf9dd8b5468109b5a793f74777bf8d2f176d
dontyouo <danchuodanchuo@gmail.com>
2022-03-28 12:07:38 +0300

    10.C done

--- #10 Coding-2/C_MessageEncoding/main.cpp
+++ #10 Coding-2/C_MessageEncoding/main.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int calculatePossibleK(int number_of_bits, const std::string &bits) {
-    int unit_counter;
+    int unit_counter = 0;
     int blocks = 0;
+    std::vector<int> units(bits.length());
 
+    for (int i = 0; i < bits.length(); ++i) {
+        units[i] = unit_counter;
+
+        if (bits[i] == '1') {
+            ++unit_counter;
+        }
+    }
+
+    bool is_length_correct;
     for (int block_length = 1; block_length < number_of_bits; ++block_length) {
-        unit_counter = 0;
+        is_length_correct = true;
 
-        for (int inner_index = 0; inner_index < bits.length(); ++inner_index) {
-            if (((inner_index % block_length) != 0) || (inner_index == 0)) {
-                if (bits[inner_index] == '1') {
-                    ++unit_counter;
-                }
-            } else {
-                if (unit_counter % 2 != bits[inner_index] - '0') {
-                    break;
-                } else {
-                    if (bits.length() - inner_index <= block_length + 1) {
-                        ++blocks;
-                    }
-                }
+        for (int inner_index = block_length; inner_index < bits.length();
+             inner_index += block_length + 1) {
+            if (bits[inner_index] - '0' != units[inner_index] % 2) {
+                is_length_correct = false;
+                break;
             }
+        }
+
+        if (is_length_correct) {
+            ++blocks;
         }
     }
 
