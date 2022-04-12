b2c008f6c88fe2425abd5562ad2b169479a39c71
dontyouo <danchuodanchuo@gmail.com>
2022-04-12 17:25:58 +0300

    11.B wa3 v2

--- #11 Dynamic programming-1/B_FindingTheLengthOfASubsequence/main.cpp
+++ #11 Dynamic programming-1/B_FindingTheLengthOfASubsequence/main.cpp
@@ -1,52 +1,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
-std::vector<std::vector<int>> *createTable(int n, int m) {
-    auto table = new std::vector<std::vector<int>>(n);
-
-    for (int i = 0; i < n; ++i) {
-        table->at(i) = std::vector<int>(m);
-    }
-
-    return table;
-}
-
-void assembleLcs(const std::string &first, const std::string &second,
-                 std::vector<std::vector<int>> *table, int i, int j, int *answer) {
-    if (table->at(i).at(j) == 0) {
-        return;
-    }
-
-    if (first[i - 1] == second[j - 1]) {
-        ++(*answer);
-        return assembleLcs(first, second, table, i - 1, j - 1, answer);
-    }
-
-    if (table->at(i - 1).at(j) < table->at(i).at(j - 1)) {
-        return assembleLcs(first, second, table, i, j - 1, answer);
-    }
-
-    return assembleLcs(first, second, table, i - 1, j, answer);
-}
-
-void calculateTable(const std::string &first, const std::string &second,
-                    std::vector<std::vector<int>> *table) {
-    for (int index = 0; index < table->size(); ++index) {
-        for (int jj = 0; jj < table->at(0).size(); ++jj) {
-            if (jj == 0 || index == 0) {
-                continue;
-            }
-
-            if (first[index] == second[jj]) {
-                table->at(index).at(jj) = table->at(index - 1).at(jj - 1) + 1;
+int calculateMaximumLengthOfASubsequence(const std::string &first, const std::string &second) {
+    std::vector<int> lcs(second.length());
+    int previous_data;
+    int counter = 0;
+    for (int index = 1; index < first.length(); ++index) {
+        for (int j_index = 1; j_index < second.length(); ++j_index) {
+            previous_data = lcs[j_index];
+            if (first[index] == second[j_index]) {
+                lcs[j_index] = counter + 1;
             } else {
-                table->at(index).at(jj) =
-                    std::max(table->at(index - 1).at(jj), table->at(index).at(jj - 1));
+                if (lcs[j_index] >= lcs[j_index - 1]) {
+                    lcs[j_index] = lcs[j_index];
+                } else {
+                    lcs[j_index] = lcs[j_index - 1];
+                }
             }
+            counter = previous_data;
         }
     }
+
+    return lcs[second.length() - 1];
 }
 
 int main() {
@@ -59,15 +35,11 @@ int main() {
     std::getline(std::cin, first);
     std::getline(std::cin, second);
 
-    auto table = createTable(first.length() + 1, second.length() + 1);
-
-    calculateTable(first, second, table);
-
-    int answer = 0;
-    assembleLcs(first, second, table, first.length(), second.length(), &answer);
+    int answer = first.length() > second.length()
+                     ? calculateMaximumLengthOfASubsequence(first, second)
+                     : calculateMaximumLengthOfASubsequence(second, first);
 
     std::cout << answer;
 
-    delete table;
     return 0;
 }
