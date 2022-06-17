eee934f3ef3283bf1e35d063e5eb4625c1d825ed
dontyouo <danchuodanchuo@gmail.com>
2022-06-17 17:03:42 +0300

    18.A wa13 v2

--- #18 Combinatorics-2/A_Tents/main.cpp
+++ #18 Combinatorics-2/A_Tents/main.cpp
@@ -2,22 +2,41 @@
 #include <vector>
 #include <algorithm>
 
-void printVector(std::vector<int> *vector, int limit) {
-    for (int i = 0; i < limit; ++i) {
+void printVector(std::vector<int> *vector) {
+    for (int i = 0; i < vector->size(); ++i) {
         std::cout << vector->at(i) << ' ';
     }
     std::cout << '\n';
 }
 
-void partialPermutation(std::vector<int> *answer, int position, int max, int length) {
-    if (length == 0) {
-        printVector(answer, position);
-    } else {
-        for (int i = 1; i <= std::min(max, length); ++i) {
-            answer->at(position) = i;
-            partialPermutation(answer, position + 1, i, length - i);
+void partialPermutation(int n) {
+    auto answer = new std::vector<int>(n, 1);
+    printVector(answer);
+    int sum;
+    int current;
+
+    while (answer->size() > 1) {
+        sum = answer->at(answer->size() - 1);
+        answer->pop_back();
+        current = answer->size() - 1;
+
+        while (current > 0 && answer->at(current - 1) == answer->at(current)) {
+            sum += answer->at(answer->size() - 1);
+            answer->pop_back();
+            --current;
         }
+
+        ++answer->at(current);
+        --sum;
+
+        for (int i = 0; i < sum; ++i) {
+            answer->push_back(1);
+        }
+
+        printVector(answer);
     }
+
+    delete answer;
 }
 
 int main() {
@@ -28,9 +47,7 @@ int main() {
 
     std::cin >> n;
 
-    std::vector<int> answer(n);
-
-    partialPermutation(&answer, 0, n, n);
+    partialPermutation(n);
 
     return 0;
 }
